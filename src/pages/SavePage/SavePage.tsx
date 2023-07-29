33d7ebe9499d110e0053629e72365b0c710b4268
Serhii Pokhylko <serhii.pokhylko@eleks.com>
2023-07-30 02:28:39 +0300

    feat: added Save page

--- src/pages/SavePage/SavePage.tsx
+++ src/pages/SavePage/SavePage.tsx
@@ -1,5 +1,41 @@
 const SavePage = () => {
-	return <h1>Save Page</h1>
+	return (
+		<article className="main">
+			<div className="hero">
+				<div className="hero-cards _container">
+					<span className="massadge">Віаємо!</span>
+					<span className="massadge">Хочете зберегти пам’ятку культури, архітектури, мистецтва?</span>
+					<span className="massadge">Перше: будьякі пам'ятки історії - можуть стати Об'єктом Кульурної Спадщини (ОКС);</span>
+					<span className="massadge">Друге: Якщо об’єкт не має статусу ОКС (клікніть на нього на мапі, щоб побачити), ви можете ініціювати надання йому такого статусу;</span>
+					<span className="massadge">Третє: саме надання статусу ОКС є головним для розвитку та збереження пам'тки архітектури;</span>
+					<span className="massadge">Тоді рушаймо по кроках!</span>
+					<span className="massadge">АЛГОРИТМ: 1.Історичний опис + Сучасна фотофіксація -&gt; 2. Підписи -&gt; 3. Формування подання -&gt; 4. Внесення до Переліку -&gt; 5.Прийняття на Облік -&gt; Надання об'єкту статусу ОКС -&gt; Укладання охоронного договору;</span>
+				</div>
+			</div>
+			<div className="video">
+				<div className="video-link">
+					<p className="video-name">Взяття на облік об’єкта культурної спадщини<span className="date"></span></p>
+					<a className="btn-video" href="https://zakon.rada.gov.ua/laws/show/z0528-13#Text">Дивитись</a>
+				</div>
+				<div className="video-link">
+					<p className="video-name">Занесення до Переліку об’єктів культурної спадщини - є підгрунтям	<span className="date"> </span></p>
+					<a className="btn-video" href="https://docs.google.com/document/d/1GqKPfQ3tX236DPPTFYAAK04GRE65ChUjAtJnvtB1a3Y/edit?usp=sharing">Документ</a>
+				</div>
+				<div className="video-link">
+					<p className="video-name">ПОДАННЯ<span className="date"> </span></p>
+					<a className="btn-video" href="https://docs.google.com/document/d/1OK_hf1fLj4jgyh5cs1HmFvIZOBNzGQKqyzprLw5PNCM/edit?usp=sharing">Документ</a>
+				</div>
+				<div className="video-link">
+					<p className="video-name">Історичний опис та фотофіксація<span className="date"> </span></p>
+					<a className="btn-video" href="#">Дивитись</a>
+				</div>
+				<div className="video-link">
+					<p className="video-name">Підписи<span className="date"> </span></p>
+					<a className="btn-video" href="#">Дивитись</a>
+				</div>
+			</div>
+		</article>
+	);
 };
 
 export { SavePage };
