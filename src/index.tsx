feb91f806687e4abaf650a67b6984db9befb39da
Serhii Pokhylko <serhii.pokhylko@eleks.com>
2023-07-30 02:31:27 +0300

    style: added global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
+import './styles.css';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
