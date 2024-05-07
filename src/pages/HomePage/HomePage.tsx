d3a5080be5730d75c540fe56fe7a97320cfc998b
Kilkenni <cgustu@gmail.com>
2024-05-07 20:57:03 +0300

    began refactor Map to react-map-gl

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,39 @@
 import { Map } from '../../components';
+import {useEffect, useState} from 'react';
+import GlMap from 'react-map-gl/maplibre';
 
-const HomePage = () => {
-	return <Map />
+const contStyle = {
+	display: "flex",
+	width: "calc(100%)",
+	height: "100%"
+}
+
+function HomePage() {
+	const [style, setStyle] = useState('https://tile.openstreetmap.org.ua/styles/positron-gl-style/style.json');
+	const[styleJson, setStyleJson] = useState(null);
+
+	useEffect(() => {
+		async function fetchStyle() {
+			const response = await fetch(style);
+    		const jsonData = await response.json();
+			setStyleJson(jsonData);
+		};
+
+		fetchStyle();
+	}, [style]);
+
+	return <div style={contStyle}>
+			<Map />
+			
+			{styleJson?<GlMap
+				initialViewState={{latitude: 48.4701, longitude: 35.0064, zoom: 10}}
+				interactive={true}
+				//style={contStyle}
+				mapStyle={styleJson}
+			/>:"Loading"}
+
+
+		</div>
 };
 
 export { HomePage };
