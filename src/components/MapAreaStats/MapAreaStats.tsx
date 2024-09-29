5d1705bd94d8b1c0320c1dc5336c271eeba24579
Kilkenni <cgustu@gmail.com>
2024-09-29 20:59:58 +0300

    area stats ignore clicks

--- src/components/MapAreaStats/MapAreaStats.tsx
+++ src/components/MapAreaStats/MapAreaStats.tsx
@@ -56,9 +56,9 @@ export function MapAreaStats({ areas, children }: MapAreaStatsProps) {
 
   // console.log(icons)
   
-  return <div className=" bg-white bg-opacity-75 px-3 py-1 rounded-xl md:bg-opacity-0 md:p-0 relative flex flex-row max-w-60 md:max-w-3xl h-13 mx-auto mt-3 md:mt-11 justify-between font-sans" >
+  return <div className=" bg-white bg-opacity-75 px-3 py-1 rounded-xl md:bg-opacity-0 md:p-0 relative flex flex-row max-w-60 md:max-w-3xl h-13 mx-auto mt-3 md:mt-11 justify-between font-sans pointer-events-none" >
     {areaStats.map((areaCategory) => (
-      <div className='inline-block md:block basis-48 bg-white bg-opacity-0 md:bg-opacity-75 md:px-3 md:py-1 md:rounded-xl' key={areaCategory.categoryName}>
+      <div className='inline-block md:block basis-48 bg-white bg-opacity-0 md:bg-opacity-75 md:px-3 md:py-1 md:rounded-xl pointer-events-auto' key={areaCategory.categoryName}>
         <span className='block text-center text-2xl'>
           <span className='font-bold'>{areaCategory.quantity}</span>
           <span className='hidden md:inline'> | {Math.round(areaCategory.surface / 10000)}</span>
