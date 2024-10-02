7d920bb2bbf3753ff5b2df34d366483fed4e231e
Andrii Holovin <Andygol@users.noreply.github.com>
2024-10-02 21:12:31 +0300

    Remove console.log

--- src/components/utils/trackingUtils.tsx
+++ src/components/utils/trackingUtils.tsx
@@ -1,7 +1,6 @@
 import ReactGA from 'react-ga4';
 
 export const trackEvent = (category: string, action: string, label: string, value?: number) => {
-  console.log(`${label} button clicked`);
   ReactGA.event({
     category,
     action,
