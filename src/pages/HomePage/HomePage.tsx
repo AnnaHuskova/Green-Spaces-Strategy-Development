90346292c41ae14abd28a3f16626e3e01a3d2e0e
Kilkenni <cgustu@gmail.com>
2024-10-06 23:02:51 +0300

    moved map source to separate container

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -369,8 +369,11 @@ function HomePage({greenAreas, districts}: HomePageProps) {
           </AreaFilterRadio>
           
         </div>
-        <MapSourceSwitch sources={availableStyles} selectedSource={style} onSetSource={setStyle} />
+        {/* <MapSourceSwitch sources={availableStyles} selectedSource={style} onSetSource={setStyle} /> */}
         
+      </MapLegend>}
+      {<MapLegend className={"fixed max-md:bottom-0 md:absolute md:bottom-0 left-0 w-full md:w-auto md:min-h-14 min-w-14 md:max-h-[calc(100%-28px)] overflow-y-scroll bg-white bg-opacity-75 md:py-1 md:px-4 md:rounded-xl shadow-sm"}>
+        <MapSourceSwitch sources={availableStyles} selectedSource={style} onSetSource={setStyle} />
       </MapLegend>}
       <MapAreaStats areas={greenAreas}></MapAreaStats>
       {areaInfo.data && areaInfo.extended === false &&
