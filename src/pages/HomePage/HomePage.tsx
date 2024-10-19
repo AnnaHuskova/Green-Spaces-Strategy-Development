29c1da80b8c57d3aae8eb65cb0c63d5d4282a87f
Kilkenni <hpittman89@outlook.com>
2024-10-19 14:52:44 +0300

    workaround: hide tile select on mobile until fixed

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -372,7 +372,7 @@ function HomePage({greenAreas, districts}: HomePageProps) {
         {/* <MapSourceSwitch sources={availableStyles} selectedSource={style} onSetSource={setStyle} /> */}
         
       </MapLegend>}
-      {<MapLegend className={"fixed max-md:bottom-0 md:absolute md:bottom-0 left-0 w-full md:w-auto md:min-h-14 min-w-14 md:max-h-[calc(100%-28px)] overflow-y-auto bg-white bg-opacity-75 md:py-1 md:px-4 md:rounded-xl shadow-sm"}>
+      {<MapLegend className={"fixed hidden md:block max-md:bottom-0 md:absolute md:bottom-0 left-0 w-full md:w-auto md:min-h-14 min-w-14 md:max-h-[calc(100%-28px)] overflow-y-auto bg-white bg-opacity-75 md:py-1 md:px-4 md:rounded-xl shadow-sm"}>
         <MapSourceSwitch sources={availableStyles} selectedSource={style} onSetSource={setStyle} />
       </MapLegend>}
       <MapAreaStats areas={greenAreas}></MapAreaStats>
